5c4474bc026b752f460251cb87dae9d16c487c10
Yves <yves.as@gmail.com>
2025-03-23 11:12:20 -0300

    :broom: cleanup: remove console.log

--- frontend/src/functions/api.ts
+++ frontend/src/functions/api.ts
@@ -1,7 +1,6 @@
 const baseURL = process.env.NEXT_PUBLIC_API_URL
 
 export async function httpGet(url: string) {
-	console.log(normalizarUrl(`${baseURL}/${url}`))
 	const response = await fetch(normalizarUrl(`${baseURL}/${url}`))
 	return response.json()
 }
